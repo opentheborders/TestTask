19f1f89b2f295a55d49924544943c4a8ef8fa1c5
Niklas <mrnikolayzv@gmail.com>
2021-11-23 12:52:45 +0200

    task 1.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-    }
-}
+        int n = 50;
+        double[] array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+        System.out.println(Arrays.toString(array));
+        double max = Arrays.stream(array)
+                .reduce((a, b) -> Math.max(a, b))
+                .getAsDouble();
+        double min = Arrays.stream(array)
+                .reduce((a, b) -> Math.min(a, b))
+                .getAsDouble();
+        OptionalDouble avg = Arrays.stream(array).average();
+        System.out.println(max + "\n" + min + "\n" + avg);
+    }};
+
