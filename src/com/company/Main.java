e000b40f204b8219fa6899d076960ea4737b34e4
Niklas <mrnikolayzv@gmail.com>
2021-11-24 14:49:23 +0200

    task 1.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,29 @@ import java.util.Arrays;
 import java.util.OptionalDouble;
 
 public class Main {
+    public static int[] compare(int array[], int number, int n){
 
+            int counter = 0;
+        for(int i=0; i< array.length; i++){
+            {
+                if (array[i] == number) {
+                  counter++;
+                } else array[i - counter]  = array[i];
+            }}
+
+        int []array1 = new int [n-counter];
+        for(int i=0; i<array1.length; i++){
+            array1[i] = array[i];
+        }
+        System.out.println(Arrays.toString(array1));
+        return array1;
+    }
     public static void main(String[] args) {
 
-        for (int i = 2; i <= 100; i++) {
-            for (int j = 2; j <= i; j++) {
-                if (j < i  && i%j == 0) {
-                    break;
-                } else
-                if (i % j == 0) {
-        System.out.println(i);
+        int []array = {4,5,2,8,9,1,5,8};
+        int n = array.length;
+        int number = 5;
+        compare(array,number,n);
 
-        }}}}}
+        }}
 
