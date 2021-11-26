3e855050da7608a0af7e35d07ff7e30c3be653ff
Niklas <mrnikolayzv@gmail.com>
2021-11-26 16:37:41 +0200

    1.0 //Changes

--- src/com/company/Filtering.java
+++ src/com/company/Filtering.java
@@ -12,18 +12,10 @@ import java.util.OptionalDouble;
 class Filtering {
 
     public static void main(String[] args) {
-        int n = 50;
-        double[] array = new double[n];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Math.random();
-        }
-        System.out.println(Arrays.toString(array));
-        double max = Arrays.stream(array)
-                .reduce((a, b) -> Math.max(a, b))
-                .getAsDouble();
-        double min = Arrays.stream(array)
-                .reduce((a, b) -> Math.min(a, b))
-                .getAsDouble();
-        OptionalDouble avg = Arrays.stream(array).average();
-        System.out.println(max + "\n" + min + "\n" + avg);
+
+  int n = 5;
+  double []array = new double [n];
+
+  new ArrayUpdate(array);
+
     }}
