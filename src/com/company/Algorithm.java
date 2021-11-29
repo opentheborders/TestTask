83c47eec374fe9fecebfa4a45210681df1ff8072
Niklas <mrnikolayzv@gmail.com>
2021-11-29 14:16:50 +0200

    1.1(Remastered) // braces

--- src/com/company/Algorithm.java
+++ src/com/company/Algorithm.java
@@ -6,23 +6,23 @@ import java.util.OptionalDouble;
 1.1. Реализуйте алгоритм сортировки пузырьком для сортировки массива
  */
 
-public class Algorithm {
+public class Algorithm{
+    static void sort(int[] array) {
 
-     static void sort(int[] array) {
-
-         for(int i = array.length-1 ; i > 0 ; i--){
-
-             for(int j = 0 ; j < i ; j++){
+        for(int i = array.length-1 ; i > 0 ; i--) {
+            for(int j = 0 ; j < i ; j++){
                 if( array[j] > array[j+1] ){
-                int tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
             }
-            }}
-            }
-                public static void main(String[] args) {
+        }
+    }
+    public static void main(String[] args) {
 
-                    int [] array = {5,4,8,7,9,3};
-                    sort(array);
-                    System.out.println(Arrays.toString(array));
-                }}
+        int [] array = {5,4,8,7,9,3};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
